941c79167598806d8d5f35edf321b47b86d78197
manubalieiro <128410558+manubalieiro@users.noreply.github.com>
2023-09-20 16:30:36 -0300

    Create unavailable-bike-error.ts

---
+++ unavailable-bike-error.ts
@@ -0,0 +1,8 @@
+// unavailable-bike-error.ts
+export class UnavailableBikeError extends Error {
+  public readonly name = 'UnavailableBikeError';
+
+  constructor() {
+    super('Unavailable bike.');
+  }
+}
